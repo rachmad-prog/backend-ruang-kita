9175972797615d2b763a6a8fcf9cd36698cdf7cf
Rachmad-Prog <rachmadmap@gmail.com>
2026-07-03 22:36:54 +0700

    kedua

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 
-const { testConnection } = require('./src/config/db');
-const authRoutes = require('./src/routes/authRoutes');
-const roomRoutes = require('./src/routes/roomRoutes');
-const bookingRoutes = require('./src/routes/bookingRoutes');
-const paymentRoutes = require('./src/routes/paymentRoutes');
-const { errorHandler, notFound } = require('./src/middleware/errorHandler');
+const { testConnection } = require("./src/config/db");
+const authRoutes = require("./src/routes/authRoutes");
+const roomRoutes = require("./src/routes/roomRoutes");
+const bookingRoutes = require("./src/routes/bookingRoutes");
+const paymentRoutes = require("./src/routes/paymentRoutes");
+const { errorHandler, notFound } = require("./src/middleware/errorHandler");
 
 const app = express();
 
-app.use(cors({ origin: process.env.CLIENT_URL || 'http://localhost:5173' }));
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "https://ruang-kita-steel.vercel.app",
+  }),
+);
 app.use(express.json());
 
 // Foto room yang diupload admin bisa diakses lewat http://localhost:5000/uploads/rooms/namafile.jpg
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-app.get('/api/health', (req, res) => {
-  res.json({ status: 'ok', message: 'Booking API berjalan.' });
+app.get("/api/health", (req, res) => {
+  res.json({ status: "ok", message: "Booking API berjalan." });
 });
 
-app.use('/api/auth', authRoutes);
-app.use('/api/rooms', roomRoutes);
-app.use('/api/bookings', bookingRoutes);
-app.use('/api/payments', paymentRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/rooms", roomRoutes);
+app.use("/api/bookings", bookingRoutes);
+app.use("/api/payments", paymentRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
@@ -34,6 +38,8 @@ const PORT = process.env.PORT || 5000;
 
 testConnection().then(() => {
   app.listen(PORT, () => {
-    console.log(`🚀 Server berjalan di http://localhost:${PORT}`);
+    console.log(
+      `🚀 Server berjalan di https://ruang-kita-steel.vercel.app:${PORT}`,
+    );
   });
 });
